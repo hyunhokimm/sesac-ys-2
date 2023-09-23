c707ed980fc7b4e4b682402ca719db5259a11941
hyunhokimm <whoaukim@gmail.com>
2023-09-23 15:45:15 +0900

    문자열 내장 메소드

---
+++ 2week/htmlCssJs/js/js_arr.js
@@ -0,0 +1,31 @@
+let arr = [1,2,3,4,5,6]
+
+arr.push(9)
+
+arr.shift()
+arr.unshift(4)
+
+console.log(arr)
+
+console.log(arr.includes(4))
+
+console.log(arr.indexOf(3))
+
+arr.reverse()
+
+console.log(arr.join('*'))
+
+console.log(arr)
+
+let str = 'Happy day!~ '
+console.log(str.toUpperCase())
+console.log(str)
+
+console.log(str.slice())
+
+str = str.split('').reverse().join('').toUpperCase()
+
+console.log(str)
+
+ let num = str.indexOf('H')
+ console.log(num)
