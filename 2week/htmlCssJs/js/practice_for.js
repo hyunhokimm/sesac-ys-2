3bf6c62bed21f2a7d2ce71bcb5144027d6e06b72
hyunhokimm <whoaukim@gmail.com>
2023-09-23 14:30:14 +0900

    for문 실습

--- 2week/htmlCssJs/js/practice_for.js
+++ 2week/htmlCssJs/js/practice_for.js
@@ -7,3 +7,34 @@ for(i = 1 ; i <= nums; i++){
 }
 
 
+let numbers =[]
+for(let i = 1 ; i <=100 ; i++){
+    numbers.push(i)
+}
+console.log(numbers)
+
+
+//for문 실습
+let j = 0
+for(i = 0; i< 100; i++){
+    j +=numbers[i]
+}
+console.log(j)
+
+
+//for of 문 실습
+let k = 0;
+for(num of numbers){
+    k += num
+}
+console.log(k)
+
+
+//for each 문 실습
+
+let l = 0;
+numbers.forEach((num)=>{
+    l += num
+})
+
+console.log(l)
