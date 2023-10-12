ed164309235c520af2ca0cfb263166a471c1d4c3
hyunhokimm <whoaukim@gmail.com>
2023-10-12 13:47:43 +0900

    노드모듈

---
+++ 5week/node/index_es6.js
@@ -0,0 +1,8 @@
+import PI, { add, minus } from "./math_es6.js";
+// import  from "./math_es6.js";
+
+add(5, 4);
+
+minus(3, 1);
+
+console.log(PI);
---
+++ 5week/node/math_es6.js
@@ -0,0 +1,12 @@
+const add = (a, b) => {
+  console.log(a + b);
+};
+
+const minus = (a, b) => {
+  console.log(a - b);
+};
+
+const PI = 3.14;
+
+export default PI;
+export { add, minus };
