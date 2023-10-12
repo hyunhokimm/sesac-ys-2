51290b05800809cd70aab9d25d8cd61ff7ea71a0
hyunhokimm <whoaukim@gmail.com>
2023-10-12 15:21:00 +0900

    ejs

--- 5week/backend/01-express/index.js
+++ 5week/backend/01-express/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8000;
 
+app.set("view engine", "ejs");
+// views 폴더는 디폴트값
+app.set("views", "./views");
+
+come = 0;
 app.get("/", (req, res) => {
-  console.log("hhhh");
-  res.send("hello");
+  come += 1;
+  console.log(`${come}명이 방문하였습니다.`);
+  res.status(200).send("hello~~~");
+});
+
+app.get("/ejs", (req, res) => {
+  // render 메소드의 기본 디렉토리는 app.set의 셋팅값
+  res.render("index");
+});
+
+app.get("/test", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 app.listen(port, () => {
