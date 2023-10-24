4e277b0e45713eb25d66791d012318b5c379cba3
hyunhokimm <whoaukim@gmail.com>
2023-10-24 18:52:17 +0900

    sql

---
+++ 7week/database/sql심화.sql
@@ -0,0 +1,26 @@
+-- < selet문 심화 >
+-- customer 테이블에 존재하는 addr의 종류를 알고 싶다.
+select distinct addr from customer;
+
+-- orders 테이블에 존재하는 주문 개수 
+select count(*) as 'total_orders' from orders;
+
+-- 사람별 주문 건수
+-- select에서 group by를 쓸 때, group by뒤에 쓴 속성과 집계함수로 새로 만든 속성만 출력. (다른 prodname 등등의 속성은 출력 X)
+select custid, count(*) as 'count' from orders group by custid;
+
+-- 사람별로 구매한 상품의 개수
+select * from orders;
+select custid, sum(amount) as 'total_amount' from orders group by custid;
+
+-- 사람별로 구매한 상품의 개수 조회하는데, 구매한 상품의 개수가 5개 이상인 경우만 조회 (custid != 'bunny'인 사람)
+select custid, sum(amount) as 'total_amount' from orders 
+where custid != 'bunny' 
+group by custid 
+having sum(amount) >= 5;
+
+-- 사람별로 결제한 금액
+select custid, sum(amount*price) as 'total_price' from orders group by custid;
+
+-- customer, orders을 inner join > 주문별로 배송지를 알고 싶어서
+select customer.addr, orders.* from customer inner join orders on customer.custid = orders.custid;
